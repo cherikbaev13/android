894fae743336359b44ecef132893c6e104328094
Cherikbaev Tilek <cherikbaev13@gmail.com>
2019-04-16 20:37:27 +0600

    First commit

---
+++ app/src/main/java/com/example/fragmentex/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fragmentex;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
